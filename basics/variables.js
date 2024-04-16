3507b897a0c93842a17acc68364540b3ed4c6ac9
sarinashresthaa <166709968+sarinashresthaa@users.noreply.github.com>
2024-04-16 10:13:04 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountID=11243
+let accountEmail="sarina@gmail.com"
+var accountPassword="12345"
+accountCity="brt"
+let accountState;
+
+//accountID=2;
+accountEmail="s@gmail.com"
+accountPassword="2121"
+accountCity="ktm"
+
+/*prefer not to use var because of issues in block scope and functional scope*/
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
