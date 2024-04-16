b6cf5d253a239c74671cb4e35a9cd3ad1ce5a287
Sarina Shrestha <166709968+sarinashresthaa@users.noreply.github.com>
2024-04-16 11:16:32 +0000

    Conversion of datatypes

---
+++ basics/conversionoperation.js
@@ -0,0 +1,25 @@
+let score="20abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"20"=> 20
+//"20abc"=> NaN(NotaNumber)
+//true=> 1, false=>0
+
+let isLoggedIn="sarina"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true, 0=> false
+//""=> false, "sarina"=> true
+
+let somenumber=22
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
