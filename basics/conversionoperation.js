3481f5c958ce1f83b12ad3a90c8ee38b29d07e91
Sarina Shrestha <166709968+sarinashresthaa@users.noreply.github.com>
2024-04-16 11:44:45 +0000

    operation

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -23,3 +23,50 @@ let somenumber=22
 let stringnumber=String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+//****************** Operations ******************
+
+let value=2
+let negValue=-value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+console.log(2**3);
+
+let str1="hello"
+let str2=" sarina"
+ 
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);
+console.log(1+2+"3");
+
+console.log(1+2*3/1);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
